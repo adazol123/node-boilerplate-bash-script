fc97af46baf76d6d9d9b8d8d1eb4acac25d9a1fd
Danyel <dpadayao40@gmail.com>
2022-06-05 19:11:44 +0800

    added comment for each logic

--- node-script.sh
+++ node-script.sh
@@ -2,6 +2,8 @@
 
 
 #initializing node 
+
+# check if yarn package manager is currently installed on the machine
 if [ -x "$(command -v yarn)" ];
 then
 	yarn init
@@ -13,6 +15,8 @@ then
 	sleep 0.5
 	yarn add express discord.js mongoose
 	echo "Express,Discord,Mongoose packages successfully installed"
+
+# check if npm package manager is currently installed on the machine
 elif [ -x "$(command -v npm)" ];
 then
 	npm init
@@ -23,6 +27,7 @@ then
 	npm -i express discord.js mongoose
 	echo "Packages successfully installed using NPM"
 else
+	# return an error of both yarn or npm is not detected on the machine
 	echo "no packages manager or installer found on your machine, please try to install yarn or npm first"
 
 fi
