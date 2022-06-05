2a2bee3f42c7715a5f6de114c8ce4abc43c5e445
Danyel <dpadayao40@gmail.com>
2022-06-05 21:50:33 +0800

    added comment

--- initializer-git.sh
+++ initializer-git.sh
@@ -1,38 +1,59 @@
 #!/bin/bash
 
+#################################################################
+#								#
+#	    Creating repo using command with Github CLI		#
+#								#
+#################################################################
 
+
+# Need input from the user for repository name and its visibility
 echo "What would you name for your repository?: "
 read repository
 sleep 0.5
 echo "Repository visibility?: (private/public) "
 read visibility
 
-if [[ visibility == "private" ]]; then
+#condition statement for the visibility of the repository (private/public)
+if [[ visibility === "private" ]]; then
 		visible="--private"
-elif [[ visibility == "public" ]]; then
+elif [[ visibility === "public" ]]; then
 		visible="--public"
 else 
 		visible="--private"
-	fi
+fi
 
-echo $visible
+echo "visibility:  $visible"
+
+# Check if the Gihub CLI is currently installed on the machine
 if [ -x $(command -v gh) ]; then
+	# Create repository using the github CLI
 	gh repo create $repository $visible
 	reposit="git@github.com:adazol123/$repository"
-	gh repo clone $reposit
-	if [ -e $repository ]; then
 
-	sleep 0.5
-	cd $repository
-	touch README.md
-	touch .gitignore
-	git add .
-	echo "Commit: "
-	read commit
-	git commit -m "$commit"
-	git push origin
-	sleep 0.5
-	echo "Successfully created repository: $repository"
+	# Clone the repository 
+	gh repo clone $reposit
+
+	# Check  statement once both creation and cloning of the repository was successful
+	if [ -e $repository ]; then
+		# Add inital files like README, .gitignore then add content on it
+		cd $repository
+		touch README.md && echo "$repository" >> README.md
+		touch .gitignore && echo "node_modules" >> .gitignore
+
+
+		# Add all files from working directory to staging area
+		git add .
+
+		# Adding commit for the changes made on the  repository
+		echo "Commit: "
+		read commit
+		git commit -m "$commit"
+
+		# Pushing repo from stagin area to remote/origin  
+		git push origin
+		sleep 0.5
+		echo "Successfully created repository: $repository"
 	else
 		echo "Encountered an error when creating the repository"
 	fi
