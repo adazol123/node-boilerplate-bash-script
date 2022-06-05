42c18291c998e314939336ecca9cfdb8ab7bc61a
Danyel <dpadayao40@gmail.com>
2022-06-05 08:17:02 +0800

    Initial commit

---
+++ git-script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+echo "Commit: "
+read commit
+# add all files to staging area
+git add .
+sleep 1
+
+# add commit
+git commit -m "$commit"
+sleep 1
+echo "Commit added successfully: $commit"
+
+# push files from origin to master repository
+git push origin master
+
+echo "Done!"
+
+
+
+
+
+
+
+
+
+
+
---
+++ node-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#initializing node 
+yarn init
+
+
+# install express, discordjs and mongoose packages
+sleep 0.5
+echo "Processing installation with the basic packages..."
+sleep 0.5
+yarn add express discord.js mongoose
+echo "Express,Discord,Mongoose packages successfully installed"
+
+
+
